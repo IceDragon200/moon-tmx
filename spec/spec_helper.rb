dc33df39604524f09e89e42f48ee88c43c2ebf56
Corey Powell <mistdragon100@gmail.com>
2015-05-08 15:28:20 -0500

    Removed moon/packages require

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ Encoding.default_internal = Encoding.default_external = 'UTF-8'
 require 'codeclimate-test-reporter'
 require 'simplecov'
 require 'active_support/core_ext/string'
-require 'moon/packages'
 
 CodeClimate::TestReporter.start
 SimpleCov.start
